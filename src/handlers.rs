9b5402a2b18ffb8dec24b7a8d43f683ad224b1a9
Yusei Ito <me@yuseiito.com>
2022-09-26 23:37:25 +0900

    Change moduke visiblity

--- src/handlers.rs
+++ src/handlers.rs
@@ -2,7 +2,7 @@ mod handle_slash_commands;
 pub use handle_slash_commands::handle_slash_commands;
 
 mod handle_interactivity;
-use handle_interactivity::handle_interactivity;
+pub use handle_interactivity::handle_interactivity;
 
 mod verify_email;
-use verify_email::verify_email;
+pub use verify_email::verify_email;
