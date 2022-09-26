ea7b02e24e55bf0d554e873c6e6598b503a07884
Yusei Ito <me@yuseiito.com>
2022-09-26 23:15:52 +0900

    fmt

--- src/handlers/verify_email.rs
+++ src/handlers/verify_email.rs
@@ -1,3 +1,4 @@
+use crate::model::username::{Username, UsernameError};
 use crate::model::Config;
 use crate::slack::model::{
     message::{ActionElement, Block, ButtonStyle, Text},
@@ -6,7 +7,6 @@ use crate::slack::model::{
 use crate::slack::post_message::post_message;
 use serde::{Deserialize, Serialize};
 use std::convert::Infallible;
-use crate::model::username::{Username,UsernameError};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct EmailVerificationOptions {
@@ -37,23 +37,16 @@ pub async fn verify_email(
         "Email {} is verified for user {}({})",
         params.email, params.real_name, params.user_id
     );
- 
-    let message = match Username::try_from(params.real_name.as_str()){
-        Ok(username)=>match username_to_confirmation_message(username,&params){
-            Ok(msg)=> msg,
-            Err(res)=> return Ok(res)
+
+    let message = match Username::try_from(params.real_name.as_str()) {
+        Ok(username) => match username_to_confirmation_message(username, &params) {
+            Ok(msg) => msg,
+            Err(res) => return Ok(res),
         },
-        Err(e)=> username_error_to_error_message(e,&params)
+        Err(e) => username_error_to_error_message(e, &params),
     };
 
-
-    if let Err(e) = post_message(
-        &config.slack_bot_token,
-        &params.user_id,
-        message,
-    )
-    .await
-    {
+    if let Err(e) = post_message(&config.slack_bot_token, &params.user_id, message).await {
         error!("Failed to post message: {}", e);
         return Ok(
             warp::reply::html(generate_html("Error", "Failed to post message.(Internal server error)", "サーバ内部でのエラーです。情報メディアシステム局までお問い合わせください。ご不便をおかけして申し訳ありません。"))
@@ -86,9 +79,11 @@ fn generate_html(title: &str, subtitle: &str, desctiption: &str) -> String {
     )
 }
 
-
-pub fn username_error_to_error_message( e: UsernameError,params: &EmailVerificationOptions,)-> Message  // Title,subtitle,description{{
- {
+pub fn username_error_to_error_message(
+    e: UsernameError,
+    params: &EmailVerificationOptions,
+) -> Message // Title,subtitle,description{{
+{
     let text = match e{
         UsernameError::InvalidCharacters => format!("氏名「{}」には使用できない文字が含まれています。命名規則に準拠すれば、氏名は半角英数字のみになります。氏名の設定を変更、はじめからやり直してください。\n よくあるミス: 全角文字を含む、半角スペースを含むなど",params.real_name),
         UsernameError::Malformed(_,_,_,_)=> format!("氏名「{}」は形式が不正です。受け付けられるユーザ名は、たとえば'21jsysItoYusei'のような形式です。氏名の設定を変更後、はじめからやり直してください。",params.real_name),
@@ -96,14 +91,15 @@ pub fn username_error_to_error_message( e: UsernameError,params: &EmailVerificat
         UsernameError::UnknownDepartment(d)=> format!("氏名「{}」に規定外の局/組織名「{}」が含まれています。",params.real_name,d)
     };
 
-    return Message::Text(text)
+    return Message::Text(text);
 }
 
-
-pub fn username_to_confirmation_message(username: Username,params: &EmailVerificationOptions)-> Result<Message,warp::reply::Html<String>>{
-
+pub fn username_to_confirmation_message(
+    username: Username,
+    params: &EmailVerificationOptions,
+) -> Result<Message, warp::reply::Html<String>> {
     let message =format!("ユーザ名{}によれば、あなたは{}所属(20{}年入学)の{} {}さん(姓,名の順)です。間違いはありませんか?",&params.real_name,&username.department.to_japanese(),&username.year,&username.family_name,&username.given_name);
-    
+
     let continue_action_id = format!("{}-{}-continue", params.user_id, params.iat);
     let cancel_action_id = format!("{}-{}-cancel", params.user_id, params.iat);
     let value = InteractiveComponentValue::IsRealnameCorrectPromptAnswer {
@@ -124,12 +120,9 @@ pub fn username_to_confirmation_message(username: Username,params: &EmailVerific
         }
     };
 
-
     let blocks = vec![
         Block::Section {
-            text: Text::PlainText {
-                text: message,
-            },
+            text: Text::PlainText { text: message },
         },
         Block::Actions {
             elements: vec![
