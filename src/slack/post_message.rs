162e58c588e4109969adf40c53d83d8120697407
Yusei Ito <me@yuseiito.com>
2022-09-26 21:07:14 +0900

    Add: post message.rs

---
+++ src/slack/post_message.rs
@@ -0,0 +1,35 @@
+use super::model::{
+    Error, Message,
+};
+
+use std::collections::HashMap;
+
+pub async fn post_message(token: &str, channel: &str, message: Message) -> Result<(), Error> {
+
+
+    let client = reqwest::Client::new();
+    let url = "https://slack.com/api/chat.postMessage";
+
+    let message_type = match message {
+        Message::Text { .. } => "text",
+        Message::Blocks { .. } => "blocks",
+    }.to_string();
+
+    let message_str = serde_json::to_string(&message)?;
+    let request = HashMap::from([(
+        "channel".to_string(),channel.to_string()
+    ),(message_type,message_str)]);
+
+    let res = client
+        .post(url)
+        .header("Content-Type", "application/json")
+        .header("Authorization", format!("Bearer {}", token))
+        .form(&request)
+        .send().await?;
+
+    if res.status().is_success() {
+        Ok(())
+    } else {
+        Err(Error::SlackApiError(res.status().as_u16(),res.text().await?))
+    }
+}
