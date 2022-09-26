853874a94de0ed2ca45a66c65a919ea21fd8769d
Yusei Ito <me@yuseiito.com>
2022-09-26 23:30:46 +0900

    Add: Handler import

--- src/handlers.rs
+++ src/handlers.rs
@@ -3,3 +3,6 @@ pub use handle_slash_commands::handle_slash_commands;
 
 mod handle_interactivity;
 use handle_interactivity::handle_interactivity;
+
+mod verify_email;
+use verify_email::verify_email;
