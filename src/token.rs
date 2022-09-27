e18c8acf20fb104e63071b7ab46f6d83907e6adb
Yusei Ito <me@yuseiito.com>
2022-09-27 11:27:06 +0900

    Implement custom error type

--- src/token.rs
+++ src/token.rs
@@ -1,22 +1,34 @@
-
-use chrono::{Utc, TimeZone};
 use crate::model::Config;
+use chrono::{TimeZone, Utc};
+use thiserror::Error;
 
-pub fn verify_token(token: &str,iat:u64,email:&str,config: &Config)-> Result<(),()>{
+#[derive(Error, Debug)]
+pub enum TokenVerificationError {
+    #[error("Invalid token")]
+    Invalid,
+    #[error("Token is expired")]
+    Expired,
+}
 
- let token_basestring = format!("{}-{}-{}", iat, email, config.verify_salt);
- let computed_token = hmac_sha256::Hash::hash(token_basestring.as_bytes());
- let computed_token = hex::encode(computed_token);
+pub fn verify_token(
+    token: &str,
+    iat: u64,
+    email: &str,
+    config: &Config,
+) -> Result<(), TokenVerificationError> {
+    let token_basestring = format!("{}-{}-{}", iat, email, config.verify_salt);
+    let computed_token = hmac_sha256::Hash::hash(token_basestring.as_bytes());
+    let computed_token = hex::encode(computed_token);
 
- if computed_token != token {
-     return Err(());
- }
+    if computed_token != token {
+        return Err(TokenVerificationError::Invalid);
+    }
 
- let expire_at = Utc.timestamp(iat as i64, 0) + chrono::Duration::minutes(5);
+    let expire_at = Utc.timestamp(iat as i64, 0) + chrono::Duration::minutes(5);
 
- if expire_at.timestamp() < Utc::now().timestamp() {
-     return Err(());
- }
+    if expire_at.timestamp() < Utc::now().timestamp() {
+        return Err(TokenVerificationError::Expired);
+    }
 
- Ok(())
+    Ok(())
 }
